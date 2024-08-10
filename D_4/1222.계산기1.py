d56ac19b5bd6df6b72941a2b2a863799cec5dd4a
99minj0731 <99minj0731@naver.com>
2024-08-10 11:46:25 +0900

    dd

--- D_4/1222.계산기1.py
+++ D_4/1222.계산기1.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 
-def postfix(numbers):
+def postfix(numbers):  #후위 계산식을 만드는 함수
     stack = [0] * len(numbers)  #일단 빈 스택을 만들고
     top = -1  #일단 top을 지하에다가 저장
     result1 = []  #후위 계산식
@@ -30,25 +30,25 @@ def postfix_2(nums):   #후위 계산식을 계산하는 함수
             right_num = stack2[top] #오른쪽 숫자를 현재 탑에있는 걸로하고
             top -= 1   #탑 하나 내리고
             left_num = stack2[top]  #왼쪽 숫자를 현재 탑에있는 거
-            top -= 1   #탑 하나 내려
-            stack2[top] = right_num + left_num #그리구 거기에 두 값의 합을 넣어준다
+            # top -= 1   #탑 하나 내려
+            stack2[top] = int(right_num) + int(left_num) #그리구 거기에 두 값의 합을 넣어준다
         else:   # 스트링이지만 숫자인척하는 애들이 나온다면
             top += 1  #탑을 하나 올리고
             stack2[top] = token
 
-    return stack2[0]
+    return int(stack2[0]) + int(stack2[1])   #마지막 숫자가 더하기가 안되길래 그냥 뒤에 더해줬다
 
 
 
 
-for test_case in range(10):
+for test_case in range(1, 11):
     N = int(input())  # 101
     arr = list(input())  # ['9', '+', '8', '+', '5'...'6', '+', '6']
 
-    cal_change = postfix(arr)
+    cal_change = postfix(arr) #['9', '8', '+', '5', '+', '9' .....'+', '2', '+', '6', '+', '6']
     sum_result = postfix_2(cal_change)
 
-    print(sum_result)
+    print(f'#{test_case} {sum_result}')
 
 
 
