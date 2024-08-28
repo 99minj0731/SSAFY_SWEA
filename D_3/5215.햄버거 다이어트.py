69b5130936b859f26dbb34113aaa595e4f7d14b9
99minj0731 <99minj0731@naver.com>
2024-08-29 08:12:24 +0900

    29

---
+++ D_3/5215.햄버거 다이어트.py
@@ -0,0 +1,53 @@
+import sys
+sys.stdin = open('input.txt')
+
+def burger(k, n, sum_kcal, max_cal):
+    global max_taste
+
+    while sum_kcal < max_kcal:
+        # 현재까지 더한 값이 최대 칼로리를 넘겼다면
+        if sum_kcal > max_kcal:
+            return
+
+        # 최대 칼로리와 동일하다면
+        if sum_kcal == max_kcal:
+            max_taste += food[n][0]
+            sum_kcal += food[n][1]
+            return
+
+        # 최대 칼로리에 도달하지 못했다면
+        max_taste += food[k][0]
+        sum_kcal += food[k][1]
+        burger(k+1, n, sum_kcal)
+
+
+
+T = int(input()) #1
+
+for test_case in range(1, T + 1):
+    # N = 재료 개수, kcal = 먹을 수 있는 칼로리
+    N, max_kcal = map(int, input().split())
+
+    # food_taste_lst = []   #[100, 300, 250, 500, 400]
+    # food_cal_lst = []     #[200, 500, 300, 1000, 400]
+
+    # for _ in range(N):
+    #     food_taste, food_cal = map(int, input().split())
+    #     food_taste_lst.append(food_taste)
+    #     food_cal_lst.append(food_cal)
+
+    food = [list(map(int, input().split()))for _ in range(N)]
+    # [[100, 200], [300, 500], [250, 300], [500, 1000], [400, 400]]
+
+    max_taste = 0
+
+    burger(0, N, 0, max_kcal)
+
+'''
+food 리스트에서 각 요소의 1번째 자리에 위치한 것들을 부분집합으로 만들고
+만약 kcal가 넘는다면 탐색하지 않는다
+각 요소의 0번째 자리를 더해줘서
+제일 큰 값을 출력
+'''
+
+
