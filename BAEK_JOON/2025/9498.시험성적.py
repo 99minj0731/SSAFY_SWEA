cfa6f4e846cf96545a964714a66fa193f6847591
99minj0731 <99minj0731@naver.com>
2025-07-19 23:40:35 +0900

    9498

---
+++ BAEK_JOON/2025/9498.시험성적.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt')
+# input = sys.stdin.readlines
+
+score = int(input())
+
+if 90 <= score <= 100 : 
+    print("A")
+
+elif 80 <= score <= 89 :
+    print("B")
+
+elif 70 <= score <= 79 :
+    print("C")
+
+elif 60 <= score <= 69 :
+    print("D")
+
+else: print("F")
