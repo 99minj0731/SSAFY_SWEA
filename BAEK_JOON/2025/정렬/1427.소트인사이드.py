f40c73f27ab3364922f649fe5e2b80dc1100d653
99minj0731 <99minj0731@naver.com>
2025-09-03 12:17:40 +0900

    1427. 소트인사이드

---
+++ BAEK_JOON/2025/정렬/1427.소트인사이드.py
@@ -0,0 +1,25 @@
+'''
+배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+
+입력
+첫째 줄에 정렬하려고 하는 수 N이 주어진다. N은 1,000,000,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 자리수를 내림차순으로 정렬한 수를 출력한다.
+'''
+
+import sys
+sys.stdin = open('input.txt')
+
+N = str(input()) # 자리로 파악 가능 
+
+result = []
+for i in N :
+    i = int(i)
+    result.append(i)
+
+result.sort(reverse=True)
+
+# print("".join(sorted(input().strip(), reverse=True)))
+
+print(*result,sep="")
