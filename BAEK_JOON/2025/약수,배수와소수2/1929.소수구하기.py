f5a532618ad71f4c2d21232da88f034dd6322f89
99minj0731 <99minj0731@naver.com>
2025-09-22 12:31:19 +0900

    1929. 소수구하기

---
+++ BAEK_JOON/2025/약수,배수와소수2/1929.소수구하기.py
@@ -0,0 +1,28 @@
+'''
+M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
+
+출력
+한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+'''
+
+import sys
+sys.stdin = open('input.txt')
+
+import math
+M, N = map(int, input().split())
+
+def is_prime(number):
+    if number <= 1 :
+        return False
+    for i in range(2, int(math.sqrt(number))+ 1):
+        if number % i == 0 :
+            return False
+    return True
+    
+
+for i in range(M, N + 1):
+    if is_prime(i):
+        print(i)
